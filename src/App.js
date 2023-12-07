fa5e4e7244f14b536cabbaf533378a31cbf24f74
Raphael Matias <phael110199@gmail.com>
2023-12-07 10:21:18 -0300

    start creation of pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Company from './components/pages/Company';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Container customClass="min_height">
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/company' element={<Company />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/newproject' element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </Router>
   );
 }
 
