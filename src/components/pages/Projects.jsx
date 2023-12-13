df7c217d7d128da4a62d0c7d55ac5b15d51bbe94
Raphael Matias <phael110199@gmail.com>
2023-12-13 16:38:14 -0300

    Creation of the loading animation

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,39 +1,39 @@
-import { useLocation } from "react-router-dom"
-
-import { useState, useEffect } from "react"
-
-import Message from "../layout/Message"
-import Container from '../layout/Container'
-import LinkButton from "../layout/LinkButton"
-import ProjectCard from "../project/ProjectCard"
-
-import styles from './Projects.module.css'
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Message from "../layout/Message";
+import Container from '../layout/Container';
+import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+import Loading from "../layout/Loading";
+import styles from './Projects.module.css';
 
 function Projects() {
-
-    const [projects, setProjects] = useState([])
-
-    const location = useLocation()
-    let message = ''
+    const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false);
+    const location = useLocation();
+    let message = '';
 
     if (location.state) {
-        message = location.state.message
+        message = location.state.message;
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                console.log(data)
-                setProjects(data)
+        setTimeout(() => { // apenas para visualizar o loading já que o server é local
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
             })
-            .catch((err) => console.log(err))
-    }, [])
+                .then(resp => resp.json())
+                .then(data => {
+                    console.log(data);
+                    setProjects(data);
+                    setRemoveLoading(true);
+                })
+                .catch((err) => console.log(err));
+        }, 500);
+    }, []);
 
     return (
         <div className={styles.projects_container}>
@@ -41,7 +41,9 @@ function Projects() {
                 <h1>Meus Projetos</h1>
                 <LinkButton to="/newProject" text="Criar Projeto" />
             </div>
+
             {message && <Message type="success" msg={message} />}
+
             <Container customClass='start'>
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -53,9 +55,15 @@ function Projects() {
                             key={project.id}
                         />
                     ))}
+
+                {!removeLoading && <Loading />}
+
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados</p>
+                )}
             </Container>
         </div>
-    )
+    );
 }
 
-export default Projects
+export default Projects;
